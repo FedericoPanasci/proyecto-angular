e75f94fe15b26bdcdc2c2c1c18c9dd269e13582b
FedericoPanasci <feedeeblk@gmail.com>
2022-01-21 15:42:39 -0300

    version-module

--- src/app/features/login-redux/login-redux.module.ts
+++ src/app/features/login-redux/login-redux.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from 'src/app/material/material.module';
+import { LoginComponent } from './login.component';
 import { StoreModule } from '@ngrx/store';
 import { loginReducer } from './store/login.reducer';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from 'src/app/app.component';
+import { MenuComponent } from 'src/app/components/menu/menu.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ], 
+
   imports: [
     CommonModule,
-    StoreModule.forFeature('login', loginReducer)
+    StoreModule.forFeature('user', loginReducer),
+    MaterialModule,
+    ReactiveFormsModule,
+    loginReduxModule
   ]
 })
-export class LoginReduxModule { }
+export class loginReduxModule { }
